db03234606432db89eeda52acaab68cb9eedca35
Adam Sahid <masadamsahid@gmail.com>
2025-09-12 06:26:45 +0700

    🚀 feature: add services

--- constants/error/user.go
+++ constants/error/user.go
@@ -6,6 +6,7 @@ var (
 	ErrUserNotFound         = errors.New("user not found")
 	ErrorPasswordIncorrect  = errors.New("password incorrect")
 	ErrUsernameExist        = errors.New("username already exist")
+	ErrEmailExist           = errors.New("email already exist")
 	ErrPasswordDoesNotMatch = errors.New("password does not match")
 )
 
